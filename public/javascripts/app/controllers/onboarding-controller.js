baad8ec7f9f8ee12ffab9ac8229982ef738c48d5
lskrabonja <les235@cornell.edu>
2015-03-28 16:32:18 -0400

    Adding js so the plans change with selection and some more styling

--- public/javascripts/app/controllers/onboarding-controller.js
+++ public/javascripts/app/controllers/onboarding-controller.js
@@ -7,8 +7,32 @@ app.controller('OnboardingController', [
   'flashesFactory',
   function ($scope, $http, $location, flashesFactory) {
 
+    
+
     $scope.clearJumbotron();
 
     $scope.ready();
+
+    $('.option-select').on('click', function() {
+      var count = $('.option-select.active').length;
+      if ( $(this).hasClass('active') ) {
+        count--;
+      } else {
+        count++;
+      }
+      if ( count < 1 ) {
+        $('.plan').removeClass('highlight');
+        $('#planA').addClass('highlight');
+      } else if ( count < 3 ) {
+        $('.plan').removeClass('highlight');
+        $('#planB').addClass('highlight');
+      } else if ( count < 5 ) {
+        $('.plan').removeClass('highlight');
+        $('#planC').addClass('highlight');
+      } else {
+        $('.plan').removeClass('highlight');
+        $('#planD').addClass('highlight');
+      }
+    });
   }
 ]);
