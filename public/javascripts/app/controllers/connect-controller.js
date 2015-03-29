7c3c48e709a7d5409479c15a7788919cf5ead680
lskrabonja <les235@cornell.edu>
2015-03-29 12:29:06 -0400

    Implementing a chart

--- public/javascripts/app/controllers/connect-controller.js
+++ public/javascripts/app/controllers/connect-controller.js
@@ -7,9 +7,31 @@ app.controller('ConnectController', [
   '$cookieStore',
   'flashesFactory',
   function ($scope, $http, $location, $cookieStore, flashesFactory) {
+
+    
+
     $scope.ready();
 
     $scope.clearJumbotron();
+
+    var data = {
+      // A labels array that can contain any sort of values
+      labels: ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+      // Our series array that contains series objects or in this case series data arrays
+      series: [
+        [.9, .8, .6, .8, .7, .5, .7, .6, .5, .4, .5, .4],
+        [.2, .2, .5, .2, .2, .6, .2, .2, .2, .3, .3, .2]
+      ]
+    };
+
+    var options = {
+      low: 0,
+      showArea: true
+    };
+    // Create a new line chart object where as first parameter we pass in a selector
+    // that is resolving to our chart container element. The Second parameter
+    // is the actual data object.
+    new Chartist.Line('.ct-chart', data, options);
   }
 ]);
 
